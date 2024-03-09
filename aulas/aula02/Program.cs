2e7c0d6e3d55b24b5d61fe9d0515a9fbac83ef78
Samuel <109113326+samuxka@users.noreply.github.com>
2024-03-09 13:54:15 -0300

    aula02

--- aulas/aula02/Program.cs
+++ aulas/aula02/Program.cs
@@ -5,7 +5,12 @@ namespace Aula02{
     class Program{
 
         static void Main(string[] args){
+
             Console.WriteLine("Hello world");
+            if (args.GetLength(0)>0)
+            {
+                Console.WriteLine(args.GetValue(0));
+            }
         }
     }
 }
